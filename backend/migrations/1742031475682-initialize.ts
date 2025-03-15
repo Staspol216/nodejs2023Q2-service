d652fa384f20a14b45a5176a1f3f268d849c1e2c
Polishchuk <staspol216@gmail.com>
2025-03-15 12:43:10 +0300

    migration: update initialize migartion

--- backend/migrations/1742031475682-initialize.ts
+++ backend/migrations/1742031475682-initialize.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Initialize1733900086701 implements MigrationInterface {
-  name = 'Initialize1733900086701';
+export class Initialize1742031475682 implements MigrationInterface {
+  name = 'Initialize1742031475682';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -14,16 +14,19 @@ export class Initialize1733900086701 implements MigrationInterface {
       `CREATE TABLE "album" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "year" integer NOT NULL, "artistId" uuid, CONSTRAINT "PK_58e0b4b8a31bb897e6959fe3206" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "FavAlbum" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "albumId" uuid, CONSTRAINT "REL_9f921553947bb30b78219bbaed" UNIQUE ("albumId"), CONSTRAINT "PK_797615119394df830279eafdef3" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "FavTrack" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "trackId" uuid, CONSTRAINT "REL_bd2b8ec450a8e4b8f41e74c556" UNIQUE ("trackId"), CONSTRAINT "PK_62e1329758795cabcf8aa62c0e6" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "FavTrack" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "trackId" uuid, CONSTRAINT "REL_bd2b8ec450a8e4b8f41e74c556" UNIQUE ("trackId"), CONSTRAINT "PK_62e1329758795cabcf8aa62c0e6" PRIMARY KEY ("id"))`,
+      `CREATE TYPE "public"."user_role_enum" AS ENUM('admin', 'manager', 'client')`,
     );
     await queryRunner.query(
       `CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "login" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."user_role_enum" NOT NULL DEFAULT 'client', "version" integer NOT NULL DEFAULT '1', "createdAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "refreshToken" character varying, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "FavArtist" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "artistId" uuid, CONSTRAINT "REL_e59c22db667f0faa92ae9b176f" UNIQUE ("artistId"), CONSTRAINT "PK_76d6c79b9fd035c66b2ee104f28" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "FavAlbum" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "albumId" uuid, CONSTRAINT "REL_9f921553947bb30b78219bbaed" UNIQUE ("albumId"), CONSTRAINT "PK_797615119394df830279eafdef3" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "track" ADD CONSTRAINT "FK_b105d945c4c185395daca91606a" FOREIGN KEY ("albumId") REFERENCES "album"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
@@ -35,25 +38,25 @@ export class Initialize1733900086701 implements MigrationInterface {
       `ALTER TABLE "album" ADD CONSTRAINT "FK_3d06f25148a4a880b429e3bc839" FOREIGN KEY ("artistId") REFERENCES "artist"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
-      `ALTER TABLE "FavAlbum" ADD CONSTRAINT "FK_9f921553947bb30b78219bbaede" FOREIGN KEY ("albumId") REFERENCES "album"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "FavTrack" ADD CONSTRAINT "FK_bd2b8ec450a8e4b8f41e74c556e" FOREIGN KEY ("trackId") REFERENCES "track"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "FavArtist" ADD CONSTRAINT "FK_e59c22db667f0faa92ae9b176ff" FOREIGN KEY ("artistId") REFERENCES "artist"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
+    await queryRunner.query(
+      `ALTER TABLE "FavAlbum" ADD CONSTRAINT "FK_9f921553947bb30b78219bbaede" FOREIGN KEY ("albumId") REFERENCES "album"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "FavAlbum" DROP CONSTRAINT "FK_9f921553947bb30b78219bbaede"`,
+    );
     await queryRunner.query(
       `ALTER TABLE "FavArtist" DROP CONSTRAINT "FK_e59c22db667f0faa92ae9b176ff"`,
     );
     await queryRunner.query(
       `ALTER TABLE "FavTrack" DROP CONSTRAINT "FK_bd2b8ec450a8e4b8f41e74c556e"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "FavAlbum" DROP CONSTRAINT "FK_9f921553947bb30b78219bbaede"`,
     );
     await queryRunner.query(
       `ALTER TABLE "album" DROP CONSTRAINT "FK_3d06f25148a4a880b429e3bc839"`,
@@ -64,10 +67,11 @@ export class Initialize1733900086701 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "track" DROP CONSTRAINT "FK_b105d945c4c185395daca91606a"`,
     );
+    await queryRunner.query(`DROP TABLE "FavAlbum"`);
     await queryRunner.query(`DROP TABLE "FavArtist"`);
     await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TYPE "public"."user_role_enum"`);
     await queryRunner.query(`DROP TABLE "FavTrack"`);
-    await queryRunner.query(`DROP TABLE "FavAlbum"`);
     await queryRunner.query(`DROP TABLE "album"`);
     await queryRunner.query(`DROP TABLE "artist"`);
     await queryRunner.query(`DROP TABLE "track"`);
